65af7f842885c0e8f9f3d04e03a80d708b793ab8
Icarus <thucctin@gmail.com>
2021-10-04 23:20:33 +0700

    21-10-4

---
+++ Python/source/Array&String/21-10-4/Arrays_56_MergeIntervals.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/merge-intervals/
+# Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+# Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+#Solution: sort
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort(key=lambda x: x[0])
+
+        merged = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+            # otherwise, there is overlap, so we merge the current and previous
+            # intervals.
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
---
+++ Python/source/Array&String/21-10-4/BFS_200_Number of Islands.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/number-of-islands/
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    bfs(i,j,grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+     static int[][] dirs={{-1,0},{1,0},{0,1},{0,-1}};
+    public static void bfs(int i,int j,char[][] grid){
+    ArrayDeque<int[]> q=new ArrayDeque<>();
+    q.add(new int[]{i,j});
+    
+    while(q.size()>0){
+        int[] rem=q.remove();
+        for(int[] dir:dirs){
+            int X=rem[0]+dir[0];
+            int Y=rem[1]+dir[1];
+            if(X<0 || X>=grid.length || Y<0 || Y>=grid[0].length ||                 grid[X][Y]=='0'){
+                continue;
+            }
+            grid[X][Y]='0';
+            q.add(new int[]{X,Y});
+        }
+    }  
+}
+}
