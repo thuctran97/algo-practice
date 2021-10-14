682edb15ed5442bf9b61a526062becb29c32f1ab
Icarus <thucctin@gmail.com>
2021-10-14 23:48:31 +0700

    21-10-14

---
+++ Python/source/Tree/Tree_1448_CountGoodNodesinBinaryTree.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        num_good_nodes = 0
+        
+        # Use collections.deque for efficient popping
+        queue = deque([(root, float("-inf"))])
+        while queue:
+            node, max_so_far = queue.popleft()
+            if max_so_far <= node.val:
+                num_good_nodes += 1
+            if node.right:
+                queue.append((node.right, max(node.val, max_so_far)))
+            if node.left:
+                queue.append((node.left, max(node.val, max_so_far)))
+        
+        return num_good_nodes
