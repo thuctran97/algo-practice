30b47060fa402619f62557779751c8f6c2ebd911
Icarus <thucctin@gmail.com>
2021-12-02 04:23:36 +0700

    update

---
+++ Java/src/Tree/Tree_BinarySearchTree.java
@@ -0,0 +1,41 @@
+public class BinarySearchTree{
+    public Node root;
+    public BinarySearchTree(){
+        this.root = null;
+    }
+
+    public void insert(int val){
+        if (this.root == null)
+            this.root = new Node(val);
+        else
+            this.root.insert(val);
+    }
+}
+
+class Node{
+    public int val;
+    public Node leftChild;
+    public Node rightChild;
+
+    public Node(int val){
+        this.val = val;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    public void insert(int val){
+        Node current = this;
+        Node parent = current;
+        while (current != null){
+            parent = current;
+            if (val < current.val)
+                current = current.leftChild;
+            else
+                current = current.rightChild;
+        }
+        if(val < parent.val)
+            parent.leftChild = new Node(val);
+        else
+            parent.rightChild = new Node(val);
+    }
+}
