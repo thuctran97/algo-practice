83082b0e0f524a8ba93e37a3094c7fb44564d850
Thuc Tran <congthuc.tran@navercorp.com>
2022-04-29 06:42:23 +0700

    add problems
    Signed-off-by: Thuc Tran <congthuc.tran@navercorp.com>

---
+++ Java/src/Array/Array_CCI_isUnique.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Array_CCI_isUnique {
+    public static boolean isUnique(String s){
+
+        return true;
+
+
+
+    }
+    public static void main(String[] args) {
+        String s = "abc";
+        System.out.println(isUnique(s));
+    }
+}
